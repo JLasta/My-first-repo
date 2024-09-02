61f5e41cbbe9322fd813e3761f2f98de67ddaade
JLasta <lumapacryzeil@gmail.com>
2024-09-02 13:15:00 +0800

    with css and updated lines

--- index.js
+++ index.js
@@ -9,3 +9,46 @@ console.log("Enrolled:", student);
 document.getElementById("p1").innerHTML = "Hello " + firstname;
 document.getElementById("p2").innerHTML = "You are " + age + " years old";
 document.getElementById("p3").innerHTML = "Enrolled: " + student;
+
+/*let username = window.prompt("What's your name?")
+console.log(username);*/
+
+let username;
+document.getElementById("myButton").onclick = function(){
+    username = document.getElementById("myText").value;
+    console.log(username);
+    document.getElementById("myEnrty").innerHTML = "Hello " + username;
+}
+
+let a;
+let b;
+let c;
+
+document.getElementById("submitButton").onclick = function(){
+    a = document.getElementById("aText").value;
+    a = Number(a);
+
+    b = document.getElementById("bText").value;
+    b = Number(b);
+
+    c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+
+    document.getElementById("cLabel").innerHTML = "Side C: " + c;
+    console.log("Side C: " + c);
+
+}
+
+let count = 0;
+
+document.getElementById("decreaseBtn").onclick = function(){
+    count -= 1;
+    document.getElementById("countLabel").innerHTML = count;
+}
+document.getElementById("resetBtn").onclick = function(){
+    count = 0;
+    document.getElementById("countLabel").innerHTML = count;
+}
+document.getElementById("increaseBtn").onclick = function(){
+    count += 1;
+    document.getElementById("countLabel").innerHTML = count;
+}
